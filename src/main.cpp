feefaaa05b92f0f8a6b091e3a3eec46eeab6de2b
brmontgomery <brian@montgomerycrew.net>
2024-01-07 21:16:32 -0800

    Random List is being Generated

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <string>
-
-using namespace std;
+#include <random>
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    //get a random list of numbers between rangeBegin and rangeEnd
+    //set the begin and end values
+    const int rangeBegin = 0;
+    const int rangeEnd = 1000;
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    //set the size of the number list
+    std::vector<int> randomNumberList(rangeEnd-rangeBegin);
+
+    //generate the data by iterating over the number lists and inserting the numbers in order
+    std::generate(randomNumberList.begin(), randomNumberList.end(), [number = rangeBegin]() mutable {return number++;});
+    //shuffle the numbers after they have been added
+    std::shuffle(randomNumberList.begin(), randomNumberList.end(), std::mt19937{std::random_device{}()});
+
+
+    return 0;
 }
