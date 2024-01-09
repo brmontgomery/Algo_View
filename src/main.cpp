4995afdb17dd11269fbdddf1d7cfa9afc5621434
brmontgomery <brian@montgomerycrew.net>
2024-01-08 22:10:53 -0800

    Got the OpenGL environment set up... I think

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 #include <vector>
 #include <random>
 
+#include <GLFW/glfw3.h>
+
 int main()
 {
+    //For the Article: steps to set up random list 1. Add <random> 2. create the range using a vector 3. Populate the vector 4. Shuffle the vector
     //get a random list of numbers between rangeBegin and rangeEnd
     //set the begin and end values
     const int rangeBegin = 0;
@@ -17,6 +20,7 @@ int main()
     //shuffle the numbers after they have been added
     std::shuffle(randomNumberList.begin(), randomNumberList.end(), std::mt19937{std::random_device{}()});
 
-
+    //For the Article: steps to setup OpenGL 1. 
+    std::cout << "Hello!" << std::endl;
     return 0;
 }
